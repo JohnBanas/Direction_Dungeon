edf65e00217f8432ad67b6e18562e6bafa5a8d54
JPB <jbanas9124@gmail.com>
2021-11-01 13:49:07 -0500

    Main Menu
    - Created main menu ascii art
    - Created framework for main menu selection function

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 # This is a sample Python script.
+from art import *
+# Main menu Ascii art
+tprint(" Welcome to Direction Dungeon! ", font="fancy2", chr_ignore=True, decoration="wave3")
+print(" ")
+# Text for main title "fancy4" decoration="wave3"
+aprint("sword2")
+tprint(" Enter a number for the options below: ", font="fancy4", chr_ignore=True)
+aprint("sword2")
+print(" ")
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+option = input('''1. New Game 
+2. Leaderboards 
+''')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def show_menu(choice):
+    if choice == '1':
+        print('You have selected a new game.')
+        # game_start()
+    else:
+        print('You have selected leaderboards.')
+        # leaderboard()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+show_menu(option)
+
+
+
+
+
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
